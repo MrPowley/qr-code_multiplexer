f66f626cc75f8e26b51dc8f0dbfcaa4f40f0fa01
MrPowley <mrpowleyme@gmail.com>
2024-03-22 20:14:29 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # qr-code_multiplexer
-Trying to make an RGB qr-core to store more data than a regular BW qr-code
+
+The BW encoder takes in binary string and converts it into a straight line of 1 pixel height and the lenght of the string in width. Black represents 1 and white 0
+The RGBW encoder does the same except the colors that are changed, using the following rules:
+- "00" -> White
+- "01" -> Red
+- "10" -> Green
+- "11" -> Blue
+
+The program has 4 functions:
+- encoderbw() which is the black and white encoder
+- decoderbw() which is the black and white decoder
+- encoderrgbw() which is the Red, Green, Blue and White encoder
+- decoderrgbw() which is the Red, Green, Blue and White decoder
+
+the encoders input a binary string without spaces and the decoders input a image path
+
+For now it only makes a straight line, but when I will make it into an actual qr-code like shape, it will be twice as efficient with the RGBW version, and 3x efficient with the future RGBWCYMK: Red, Green, Blue, White, Cyan, Yellow, Magenta, Black.
+
---
+++ main.py
@@ -0,0 +1,72 @@
+#Using Python 3.12.2
+
+from PIL import Image
+
+color_code = {"00": (255, 255, 255),
+              "01": (255, 0, 0),
+              "10": (0, 255, 0),
+              "11": (0, 0, 255)}
+
+color_code_inverted = {s: c for c, s in color_code.items()}
+
+def encoderbw(data: str):
+    data_lenght = len(data)
+
+    if data_lenght  % 8 != 0:
+        raise Exception()
+
+
+    image_size = (data_lenght , 1)
+    image = Image.new("RGB", size=image_size)
+
+    for x, bit in enumerate(data):
+        if bit == "0":
+            image.putpixel((x, 0), (255, 255, 255))
+        elif bit == "1":
+            image.putpixel((x, 0), (0, 0, 0))
+        else:
+            pass
+
+    image.save("image.png")
+
+def decoderbw(image_path: str):
+    image = Image.open(image_path)
+
+    data = ""
+
+    for x in range(image.width):
+        pixel = image.getpixel((x, 0))
+        if pixel == (255, 255, 255):
+            data += "0"
+        else:
+            data += "1"
+
+def encoderrgbw(data: str):
+    data_lenght = len(data)
+
+    if data_lenght  % 8 != 0:
+        raise Exception()
+
+
+    image_size = (int(data_lenght/2) , 1)
+    image = Image.new("RGB", size=image_size)
+
+    
+
+    for x in range(0, len(data), 2):
+        sequence = f"{data[x]}{data[x+1]}"
+        image.putpixel((int(x/2), 0), color_code[sequence])
+
+    image.save("image.png")
+
+def decoderrgbw(image_path: str):
+    image = Image.open(image_path)
+
+    data = ""
+
+    for x in range(image.width):
+        sequence = image.getpixel((int(x), 0))
+        data += color_code_inverted[sequence]
+    
+    print(data)
+
